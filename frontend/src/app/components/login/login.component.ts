c808657911e66b788ea9d7ac2a062c031ce0ba34
dungurea <diana.ungureanu@centric.eu>
2024-10-09 13:00:15 +0300

    test3

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -43,7 +43,7 @@ export class LoginComponent implements OnInit {
 		this.createForm();
 		const encodedReturnUrl = this.route.snapshot.queryParams['returnUrl'] || commonRoutingConstants.dashboard;
 		this.returnUrl = decodeURIComponent(encodedReturnUrl);
-		console.log("testt")
+		console.log("testtt")
 	}
 
 	public login(): void {
