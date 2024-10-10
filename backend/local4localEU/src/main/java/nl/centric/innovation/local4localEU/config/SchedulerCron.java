5f693eb6c5461f9ae7cac8824825750017f39854
RO\rgramesc <rares.gramescu@centric.eu>
2024-10-10 10:32:42 +0200

    Added chronjob

---
+++ backend/local4localEU/src/main/java/nl/centric/innovation/local4localEU/config/SchedulerCron.java
@@ -0,0 +1,36 @@
+package nl.centric.innovation.local4localEU.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import nl.centric.innovation.local4localEU.repository.OtpCodesRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Component
+@Slf4j
+@EnableScheduling
+@RequiredArgsConstructor
+public class SchedulerCron {
+
+    private final OtpCodesRepository otpCodesRepository;
+
+    @Value("${otp.expiration.time}")
+    private String otpExpirationTime;
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+    @Scheduled(cron = "0 0 23 ? * SUN")
+    @Transactional
+    public void deleteExpiredOtps() {
+        log.info("Scheduler Delete old otp codes task started at : " + sdf.format(new Date()));
+        otpCodesRepository.deleteAllByCreatedDateBefore(LocalDateTime.now().minusMinutes(Integer.parseInt(otpExpirationTime) / 60));
+    }
+
+}
