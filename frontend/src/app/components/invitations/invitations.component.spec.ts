79a8847a6bd9513a7d7005ddda30a29a3fd0bad8
dungurea <diana.ungureanu@centric.eu>
2024-10-24 15:52:41 +0300

    tests

--- frontend/src/app/components/invitations/invitations.component.spec.ts
+++ frontend/src/app/components/invitations/invitations.component.spec.ts
@@ -6,11 +6,13 @@ import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MerchantService } from '../../services/merchant.service';
 import { MatDialog } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { PageEvent } from '@angular/material/paginator';
 
 describe('InvitationsComponent', () => {
-    let component: InvitationsComponent;
-    let fixture: ComponentFixture<InvitationsComponent>;
-    let merchantService: any;
+	let component: InvitationsComponent;
+	let fixture: ComponentFixture<InvitationsComponent>;
+	let merchantService: any;
 
 	const matDialogMock = {
 		open: jest.fn().mockReturnValue({
@@ -18,25 +20,53 @@ describe('InvitationsComponent', () => {
 		})
 	};
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [InvitationsComponent,InviteMerchantDialogComponent],
-            schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+	beforeEach(async () => {
+		global.structuredClone = jest.fn((val) => {
+			return JSON.parse(JSON.stringify(val));
+		});
+
+		merchantService = {
+			getPaginatedInvitations: jest.fn().mockReturnValue(of()),
+			countAllInvitations: jest.fn().mockReturnValue(of(12))
+		};
+
+		await TestBed.configureTestingModule({
+			declarations: [InvitationsComponent, InviteMerchantDialogComponent],
+			schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 			imports: [TranslateModule.forRoot()],
 			providers: [
 				TranslateService,
 				{ provide: MerchantService, useValue: merchantService },
 				{ provide: MatDialog, useValue: matDialogMock }
 			]
-        })
-            .compileComponents();
+		}).compileComponents();
 
-        fixture = TestBed.createComponent(InvitationsComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+		fixture = TestBed.createComponent(InvitationsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should call initData with correct parameters on page change', () => {
+		const initDataSpy = jest.spyOn(component as any, 'initData');
+		const event: PageEvent = { pageIndex: 1, pageSize: 25, length: 100 };
+
+		component.onPageChange(event);
+
+		expect(initDataSpy).toHaveBeenCalledWith(event.pageIndex, event.pageSize);
+	});
+
+	it('should open invite merchant dialog', () => {
+		component.openInviteMerchantsDialog();
+		expect(matDialogMock.open).toHaveBeenCalledWith(InviteMerchantDialogComponent, {
+			width: '560px',
+			autoFocus: false,
+			disableClose: true,
+			hasBackdrop: true,
+			restoreFocus: false
+		});
+	});
 });
